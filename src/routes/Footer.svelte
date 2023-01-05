034f55c993e0e85c090a6e76a0d6cbca2645bf9d
Betul Gurbuz <btlgrbz071@gmail.com>
2023-01-05 23:15:44 +0300

    add footer link

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -7,19 +7,19 @@
 </script>
 
 <footer>
-	<a href="">
+	<a href="https://www.linkedin.com/in/betul-gurbuz-dev/">
 		<Linkedin size={24} color="var(--app-text-color)" />
 	</a>
-	<a href="">
+	<a href="https://www.kaggle.com/badl071">
 		<Kaggle size={24} color="var(--app-text-color)" />
 	</a>
-	<a href="">
+	<a href="https://github.com/badl7">
 		<Github size={24} color="var(--app-text-color)" />
 	</a>
-	<a href="">
+	<a href="https://medium.com/@betul.gurbuz.dev">
 		<Medium size={24} color="var(--app-text-color)" />
 	</a>
-	<a href="">
+	<a href="https://twitter.com/7Betl">
 		<Twitter size={24} color="var(--app-text-color)" />
 	</a>
 </footer>
